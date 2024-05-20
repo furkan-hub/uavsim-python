4810a6a6ccedebee0cb3d9f19ab2cdc139682a39
Furkan Doğan <72547366+furkan-hub@users.noreply.github.com>
2024-05-20 17:28:12 +0300

    cordinate system applied

--- uavsim.py
+++ uavsim.py
@@ -4,6 +4,7 @@ import threading
 import time
 import os
 from cordinate_sys import *
+import math
 
 #region TK
 root = Tk()
@@ -26,21 +27,25 @@ WHITE = "#FFFFFF"
 #katsayılar
 CL = 0.290
 CD_horizontal = 0.113
-CD_vertical = 0.300
+CD_vertical = 3.8
 
 rho = 1.225 # rooo
 S = 0.180 # m^2
 
 #başlangıç değerleri
-uav_trust = 0 # newton
 uav_weight = 50 # Newton
 uav_horizontal_speed = 0 # m/s
 uav_accel_horizontal = 0 # newton
 uav_vertical_speed = 0 # m/s
 uav_vertical_accel = 0 #newton
+
+uav_accel = 0 
+uav_speed = 0#m/s
+
 uav_lift = 0
 distance = 0
 
+uav_trust = 0  # newton
 uav_roll = 0 #degres
 uav_yaw = 0 #degres
 uav_pitch = 0 #degres
@@ -57,12 +62,13 @@ t = 0
 dt = 0.1
 #endregion
 
-def lift(speed,pitch):
-    lift = (0.5 * CL * rho * S * (speed ** 2))*
-    return 
+def lift(speed):
+    lift = (0.5 * CL * rho * S * (speed ** 2))
+    return lift
 
 def drag(speed,CD):
-    return 0.5 * CD * rho * S * (speed ** 2)
+    drag = 0.5 * CD * rho * S * (speed ** 2)
+    return drag
 
 def update():
     global uav_trust
@@ -73,32 +79,49 @@ def update():
     uav_input_pitch = input_pitch.get()
     #print(uav_input_trust,uav_input_roll,uav_input_yaw,uav_input_pitch)
 
-    return [uav_trust,uav_input_roll,uav_input_yaw,uav_input_pitch]
+    return [uav_trust,uav_input_roll,uav_input_pitch,uav_input_yaw]
+
+def trust_calc(trust,pitch):
+    thrust_horizontal = trust * math.cos(pitch)
+    thrust_vertical = trust * math.cos(pitch)
+    return [thrust_horizontal,thrust_vertical]
 
 def model():
-    global uav_roll, uav_pitch, uav_yaw, uav_horizontal_speed, uav_vertical_speed, uav_accel_horizontal, loc_alt, distance
+    global uav_roll, uav_pitch, uav_yaw, uav_horizontal_speed, uav_vertical_speed, uav_accel_horizontal, loc_alt, distance, t , uav_lift,uav_speed ,uav_accel ,uav_vertical_accel,loc_lat,loc_lon
     for t in range(int(simulasyon_suresi/dt)):
         # Update thrust
         inputs = update()
-        
-        uav_trust = inputs[0]
 
+        uav_trust = inputs[0]
+        uav_pitch = inputs[2]
+        uav_yaw = inputs[3]
+
+        forces = trust_calc(uav_trust,uav_pitch)
 
         # lift (0 derece)
-        uav_lift = lift(uav_horizontal_speed)
+        uav_lift = lift(uav_speed)
+
+        #hız
+        uav_accel = (uav_trust - drag(uav_horizontal_speed, CD_horizontal)) / (uav_weight/9.81)
+        uav_speed += (uav_accel * dt)
+        #distance = uav_accel_horizontal*dt
 
         #yatay hız
-        uav_accel_horizontal = (uav_trust - drag(uav_horizontal_speed, CD_horizontal)) / (uav_weight/9.81)
+        uav_accel_horizontal = (forces[0] - drag(uav_horizontal_speed, CD_horizontal)) / (uav_weight/9.81)
         uav_horizontal_speed += (uav_accel_horizontal * dt)
-        distance = uav_accel_horizontal*dt
+        distance = uav_horizontal_speed*dt
 
         #dikey hız
-        uav_vertical_accel = (uav_lift - (drag(uav_vertical_speed, CD_vertical)+uav_weight)) / (uav_weight/9.81)
+        uav_vertical_accel = (uav_lift+forces[1] - (drag(uav_vertical_speed, CD_vertical)+uav_weight)) / (uav_weight/9.81)
         uav_vertical_speed += uav_vertical_accel * dt
 
-        #irtifa
+
+        #attitude
         loc_alt += uav_vertical_speed * dt
-        
+        cords = translate_coordinates([40.799881,30.291096],uav_yaw,distance)
+        loc_lat = cords[0]
+        loc_lon = cords[1]
+
         #sinirlar
         if loc_alt < 0:
             loc_alt = 0
@@ -118,16 +141,14 @@ def status():
     print("Yaw: ", uav_yaw)
     print("Speed-vertical: ", uav_vertical_speed)
     print("accel-vertical: ", uav_vertical_accel)
-
     print("Speed-horizontal: ", uav_horizontal_speed)
     print("accel-horizontal: ", uav_accel_horizontal)
-
     print("Drag:-vertical ", drag(uav_vertical_speed,uav_vertical_speed))
     print("Drag:-horizontal ", drag(uav_horizontal_speed,uav_horizontal_speed))
     print("Lift: ", uav_lift)
     print("Position: Lat {}, Lon {}, Alt {}".format(loc_lat, loc_lon, loc_alt))
     print("------")
-    print("mesafe: ",translate_coordinates([40.799881,30.291096],0,distance))#kordinat hesabı
+    print("mesafe: ",distance)#kordinat hesabı
     
 def loop():
     model()
@@ -142,8 +163,8 @@ input_roll.place(x=100,y=50)
 input_yaw = Scale(root, from_=0, to=360, length=200, orient=VERTICAL)
 input_yaw.place(x=175,y=50)
 
-input_pitch = Scale(root, from_=0, to=180, length=200, orient=VERTICAL)
-input_pitch.place(x=250,y=50)
+input_pitch = Scale(root, from_=-30, to=30, length=200, orient=VERTICAL)
+input_pitch.place(x=250, y=50)
 
 #endregion
 
